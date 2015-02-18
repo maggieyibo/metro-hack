716a13c32c4bb3e4b970df0cd2ddd45c3cc8e27e
jim-clark <email@jim-clark.com>
2015-02-18 11:14:45 -0800

    Resolve home.html conflicts

--- app/assets/javascripts/home-controller.js
+++ app/assets/javascripts/home-controller.js
@@ -3,17 +3,20 @@
 
   angular.module('app')
 
-  .controller('HomeController', ['$http', function($http) {
+  .controller('HomeController', ['$http', 'RouteService', function($http, RouteService) {
 
     var vm = this;
 
-    var vehPosListUrl = 'http://api.metro.net/agencies/lametro/vehicles';
+    // var vehPosListUrl = 'http://api.metro.net/agencies/lametro/vehicles';
 
-    $http.get(vehPosListUrl, {headers: {'Content-Type': 'application/json'}}).success(function(data) {
-      vm.posList = data;
+    // $http.get(vehPosListUrl, {headers: {'Content-Type': 'application/json'}}).success(function(data) {
+    //   vm.posList = data;
+    // });
+
+
+    RouteService.routes.then(function(data) {
+      vm.routes = data;
     });
-
-
 
   }]);
 
